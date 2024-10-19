7577b9d355f69e32ecfe6fc20ab7b9825cfb6ccc
Leonardo-Virginio-Rodrigues <vr.leonardo2020@gmail.com>
2024-10-19 10:45:36 -0300

    Create score Database and ranked score
    Signed-off-by: Cato6 <Cato6@acme.local>

--- code/game.py
+++ code/game.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.score import Score
 from code.const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
 from code.level import Level
 from code.menu import Menu
@@ -15,6 +16,7 @@ class Game:
     def run(self):
 
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -26,7 +28,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
 
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
